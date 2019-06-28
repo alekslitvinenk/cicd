c5e857f81f23ee6b06b4c029caed9cf738c84132
Alexander Litvinenko <alekslitvinenk@gmail.com>
2019-06-28 22:53:53 +0300

    Small fixes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # -v /var/run/docker.sock:/var/run/docker.sock  to map inner docker daemon to the outer one
 # -v /opt/cicd/repos:/opt/cicd/repos
-docker run --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /Users/alitvinenko/repos:/opt/cicd/repos \
--e DOCKER_USER="login" \
--e DOCKER_PASSWORD="password" \
+docker run --privileged -v /var/run/docker.sock:/var/run/docker.sock -v /opt/cicd/repos:/opt/cicd/repos \
+-e DOCKER_USER="user" \
+-e DOCKER_PASSWORD="login" \
 -it alekslitvinenk/cicd-pipeline
