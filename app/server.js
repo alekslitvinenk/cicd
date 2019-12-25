bb6e274fd69d08ba9b2a8d13922fcbaf72d84d18
Alexander Litvinenko <alekslitvinenk@gmail.com>
2019-12-25 21:55:40 +0200

    Add no-cache-header

--- app/server.js
+++ app/server.js
@@ -8,11 +8,6 @@ const request = require('request');
 const pathPrefix = process.argv.slice(2)[0];
 const sslFilesDir = process.argv.slice(2)[1];
 
-const options = {
-  key: fs.readFileSync(`${sslFilesDir}/private.key`),
-  cert: fs.readFileSync(`${sslFilesDir}/certificate.crt`)
-};
-
 const colorMap = {
   build: {
     passing: "green",
@@ -20,36 +15,40 @@ const colorMap = {
   },
 
   built: "green"
-}
+};
 
 const requestHandler = (req, res) => {
   if (isBadgeUrl(req.url)) {
 
-    const urlSegmenst = req.url.slice(1).split("/");
-    const badge = urlSegmenst[0];
-    const app = urlSegmenst[1];
+    const urlSegments = req.url.slice(1).split("/");
+    const badge = urlSegments[0];
+    const app = urlSegments[1];
 
     console.log(`Badge: ${badge}`);
     console.log(`App: ${app}`);
 
     fs.readFile(`${pathPrefix}/${badge}/${app}.txt`, (err, data) => {
       if (err) {
-        console.log(`Err: ${err}`)
-        render404(req, res)
+        console.log(`Err: ${err}`);
+        render404(req, res);
       } else {
         // Dirty hack to convert "data" to string and sanitize it
         const status = (data + "").replace(/[\n\r]+/g, '');
 
-        const x = request(`https://img.shields.io/badge/${badge}-${status}-${getBadgeColor(badge, status)}.svg`);
-        req.pipe(x);
-        x.pipe(res);
+        request(`https://img.shields.io/badge/${badge}-${status}-${getBadgeColor(badge, status)}.svg`, (error, response, body) => {
+          res.statusCode = response.statusCode;
+          res.setHeader('content-type', response.headers['content-type']);
+          res.setHeader('cache-control', 'no-store');
+          res.write(body);
+          res.end();
+        });
       }
     })
   } else {
-    console.log(`isBadgeUrl: false`)
-    render404(req, res)
+    console.log(`isBadgeUrl: false`);
+    render404(req, res);
   }
-}
+};
 
 function getBadgeColor(badge, status) {
   const badgeColors = colorMap[badge];
@@ -62,7 +61,7 @@ function getBadgeColor(badge, status) {
 }
 
 function isBadgeUrl(url) {
-  return url.slice(1).split("/").length == 2;
+  return url.slice(1).split("/").length === 2;
 }
 
 function render404(req, res) {
@@ -72,15 +71,21 @@ function render404(req, res) {
 }
 
 http
-  .createServer(options, requestHandler)
+  .createServer(requestHandler)
   .listen(port, () => {
       console.log(`Server running at http://localhost:${port}/`);
     }
-  )
+  );
+
+
+const options = {
+  key: fs.readFileSync(`${sslFilesDir}/private.key`),
+  cert: fs.readFileSync(`${sslFilesDir}/certificate.crt`)
+};
 
 https
   .createServer(options, requestHandler)
   .listen(securePort, () => {
       console.log(`Server running at http://localhost:${port}/`);
     }
-  )
+  );
